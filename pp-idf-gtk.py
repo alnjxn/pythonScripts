4d67caceba1881280f08d7c503669687797e6659
unknown <case1@dblaptop-PC.(none)>
2012-01-16 16:38:36 -0500

    test implementation of user open and save dialogs

--- pp-idf-gtk.py
+++ pp-idf-gtk.py
@@ -8,8 +8,74 @@ if gtk.pygtk_version < (2,3,90):
    print "PyGtk 2.3.90 or later required for this example"
    raise SystemExit
 
+class userEntry:
+    def enter_callback(self, widget, entry):
+        entry_text = entry.get_text()
+        print "Entry contents: %s\n" % entry_text
+
+    def entry_toggle_editable(self, checkbutton, entry):
+        entry.set_editable(checkbutton.get_active())
+
+    def entry_toggle_visibility(self, checkbutton, entry):
+        entry.set_visibility(checkbutton.get_active())
+
+    def __init__(self):
+        # create a new window
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_size_request(350, 100)
+        window.set_title("Search String")
+        window.connect("delete_event", lambda w,e: gtk.main_quit())
+
+        vbox = gtk.VBox(False, 0)
+        window.add(vbox)
+        vbox.show()
+        
+        label = gtk.Label(str="Enter Search String to be Replaced:")
+        vbox.pack_start(label, True, True, 0)
+        label.show()
+        
+        entry = gtk.Entry()
+        entry.set_max_length(50)
+        entry.connect("activate", self.enter_callback, entry)
+        entry.set_text("$$ReplaceThis$$")
+        entry.select_region(0, len(entry.get_text()))
+        vbox.pack_start(entry, True, True, 0)
+        entry.show()
+
+        hbox = gtk.HBox(False, 0)
+        vbox.add(hbox)
+        hbox.show()
+                                  
+        button = gtk.Button(stock=gtk.STOCK_CLOSE)
+        button.connect("clicked", lambda w: gtk.main_quit())
+        vbox.pack_start(button, True, True, 0)
+        button.set_flags(gtk.CAN_DEFAULT)
+        button.grab_default()
+        button.show()
+        window.show()
+        
+        searchString = entry.get_text()
+
+def userSave():
+	saveDialog = gtk.FileChooserDialog("Select output directory..", None, 
+	gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER, 
+	(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
+	gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+	
+	saveDialog.set_default_response(gtk.RESPONSE_OK)
+	response = saveDialog.run()
+
+#def main():
+    #gtk.main()
+    #return 0
+
+#if __name__ == "__main__":
+    #userEntry()
+    #main()
+
 # What string are we looking for?
-searchString = "$$ReplaceThis$$"
+#searchString = "$$ReplaceThis$$"
+
 # What do we want to replace it with?
 angles = range(0, 360)
 
@@ -34,7 +100,7 @@ def idfReplace(inFile):
 	idfFile.close()
 	print "Finished the whole script!"
 
-dialog = gtk.FileChooserDialog("Open..", None, 
+dialog = gtk.FileChooserDialog("Select IDF File..", None, 
 	gtk.FILE_CHOOSER_ACTION_OPEN, 
 	(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
 	gtk.STOCK_OPEN, gtk.RESPONSE_OK))
@@ -50,7 +116,10 @@ response = dialog.run()
 if response == gtk.RESPONSE_OK:
     #print dialog.get_filename(), 'selected'
     getFile = dialog.get_filename()
+    userEntry()
+    userSave()
     idfReplace(getFile)
 elif response == gtk.RESPONSE_CANCEL:
     print 'Closed, no files selected'
 dialog.destroy()
+
