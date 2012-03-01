48c51377b9be64e4b6c0dab092430445790e80ac
Alan Jackson <a.jackson@case-inc.com>
2012-02-29 22:25:18 -0500

    working tkinter version of idf pre processor

--- pp-idf-tk.py
+++ pp-idf-tk.py
@@ -1,96 +1,73 @@
 from Tkinter import *
 from tkFileDialog import askopenfilename
+from tkFileDialog import askdirectory
+from tkSimpleDialog import askstring
+import tkMessageBox
+
+root = Tk()
+
+# What do we want to replace it with?
+angles = (0, 90, 180, 270)
 
 # Provide user with dialog box to select the IDF file to process
 def userDialog():
- 	root=Tk()	
+	global getfile
 	filename = askopenfilename(
 		filetypes=[("All Files","*"),("IDF Files","*.idf")], 
 		initialdir=["C:\\"], 
-		title=["Select IDF File to Process.."])
+		title="Select IDF File to Process..")
+	if filename == "":
+		tkMessageBox.showinfo("Info", "User Cancelled")
+	else:
+		getfile = filename
+		userEntry()
 
 # Have user select the search string to be replaced
-class userEntry:
-    def enter_callback(self, widget, entry):
-        entry_text = entry.get_text()
-        searchString = entry_text
-        print "Entry contents: %s\n" % entry_text
-        userSave()
-
-    def entry_toggle_editable(self, checkbutton, entry):
-        entry.set_editable(checkbutton.get_active())
-
-    def entry_toggle_visibility(self, checkbutton, entry):
-        entry.set_visibility(checkbutton.get_active())
-
-    def __init__(self):
-        # create a new window
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.set_size_request(225, 100)
-        window.set_title("Search String")
-        window.connect("delete_event", lambda w,e: gtk.main_quit())
-
-        vbox = gtk.VBox(False, 0)
-        window.add(vbox)
-        vbox.show()
-        
-        label = gtk.Label(str="Enter Search String to be Replaced:")
-        vbox.pack_start(label, True, True, 0)
-        label.show()
-        
-        entry = gtk.Entry()
-        entry.set_max_length(50)
-        entry.connect("activate", self.enter_callback, entry)
-        entry.set_text("$$ReplaceThis$$")
-        entry.select_region(0, len(entry.get_text()))
-        vbox.pack_start(entry, True, True, 0)
-        entry.show()
-
-        hbox = gtk.HBox(False, 0)
-        vbox.add(hbox)
-        hbox.show()
-                                  
-        button = gtk.Button(stock=gtk.STOCK_OK)
-        button.connect("clicked", self.enter_callback, entry)
-        vbox.pack_start(button, True, True, 0)
-        button.set_flags(gtk.CAN_DEFAULT)
-        button.grab_default()
-        button.show()
-        window.show()
+def userEntry():
+	global searchstring
+	searchstring = askstring(
+		title="Search String",
+		prompt="Enter Search String to be Replaced",
+		initialvalue="$$ReplaceThis$$")
+	if searchstring == None:
+		tkMessageBox.showinfo("Info", "User Cancelled")
+		quit()
+	elif searchstring == "":
+		tkMessageBox.showinfo("Info", "Invalid String")
+		quit()
+	else:
+		userSave()
 
 # Provide user with dialog box to select folder where processed
 # files should be placed
 def userSave():
-	saveDialog = gtk.FileChooserDialog("Select output directory..", None, 
-	gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER, 
-	(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
-	gtk.STOCK_OPEN, gtk.RESPONSE_OK))
-	
-	saveDialog.set_default_response(gtk.RESPONSE_OK)
-	saveResponse = saveDialog.run()
-	
-	if saveResponse == gtk.RESPONSE_OK:
-		getSave = saveDialog.get_folder()
-		idfReplace(getFile)
-	elif response == gtk.RESPONSE_CANCEL:
-	    print 'Closed, no folder selected'
-	saveDialog.destroy()
+	global getsave
+	foldername = askdirectory(
+		initialdir=["C:\\"],
+		title="Select Output Directory...")
+
+	if foldername == "":
+		tkMessageBox.showinfo("Info", "User Cancelled")
+		quit()
+	else:
+		getsave = foldername + "/"
+		idfReplace(getfile)
 
 
-def idfReplace(inFile):
+def idfReplace(infile):
 	# For every replacement value in list of values:
 	for thisAngle in angles:
 	    # Open the input file
-	    idfFile = open(inFile)
+	    idfFile = open(infile)
 	    # Open the output file
-	    outFileName = str(getSave) + "output-" + str(thisAngle) + "-degree-rotation.idf";
+	    outFileName = str(getsave) + "output-" + str(thisAngle) + "-degree-rotation.idf";
 	    outFile = open(outFileName,'w')
 	    print "Now writing file:", outFileName    
 	 
 	    # For every line in the input file:
 	    for line in idfFile:
-	        # If searchString is found, replace it with thisAngle
-	        newLine = line.replace(searchString, str(thisAngle))
+	        # If searchstring is found, replace it with thisAngle
+	        newLine = line.replace(searchstring, str(thisAngle))
 	        # Write the line to the output file
 	        outFile.write(newLine)
 	    outFile.close()
@@ -98,15 +75,11 @@ def idfReplace(inFile):
 	idfFile.close()
 	print "Finished the whole script!"
 
-
-# What do we want to replace it with?
-angles = (0, 90, 180, 270)
 	
-def main():
-	gtk.main()
-	return 0
+# def main():
+# 	gtk.main()
+# 	return 0
 
 if __name__ == "__main__":
     userDialog()
-    main()
 
