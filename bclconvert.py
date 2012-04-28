06eb8f8a65fe6596813b1f654eda0dfd0c8df647
Alan Jackson <a.jackson@case-inc.com>
2012-04-28 17:51:09 -0400

    -a

---
+++ bclconvert.py
@@ -0,0 +1,37 @@
+import re
+
+def main():
+	f = open("/Users/ajackson/Desktop/epfile.idf")
+	line = f.readline()
+	x = []
+
+	while line:
+		str1 = "Construction,"
+		str2 = ";"
+		str3 = ","
+		if re.search(str1, line):
+			line = f.readline()
+			cName = line.split(str3)[0].strip()
+			line = f.readline()
+			while (not re.search(str2, line)):
+				x.append(line.split(str3)[0].strip())
+				line = f.readline()
+			x.append(line.split(str2)[0].strip())
+		else:
+			line = f.readline()
+
+	cLayers = cName + ' Layers'
+	print '"' + cName + '"' + " = CONSTRUCTION"
+	print '\tTYPE\t\t= LAYERS'
+	print '\tLAYERS\t\t= "' + cLayers + '"'
+	print '\t..'
+	print ''
+	print '"' + cLayers + '" = LAYERS'
+	print '\tMATERIAL\t\t= (',
+	for item in x:
+		print '"' + item + '",',
+	print ')'
+	print '\t..'
+	print ''
+
+main()
