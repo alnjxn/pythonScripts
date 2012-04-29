2ef7bfcec5d7fd71fd701c763e17a703f054c0cb
Alan Jackson <a.jackson@case-inc.com>
2012-04-28 20:12:02 -0400

    small updates

--- bclconvert.py
+++ bclconvert.py
@@ -13,25 +13,26 @@ def main():
 			line = f.readline()
 			cName = line.split(str3)[0].strip()
 			line = f.readline()
+			
 			while (not re.search(str2, line)):
 				x.append(line.split(str3)[0].strip())
 				line = f.readline()
 			x.append(line.split(str2)[0].strip())
+
+			cLayers = cName + ' Layers'
+			print '"' + cName + '"' + " = CONSTRUCTION"
+			print '\tTYPE\t\t= LAYERS'
+			print '\tLAYERS\t\t= "' + cLayers + '"'
+			print '\t..'
+			print ''
+			print '"' + cLayers + '" = LAYERS'
+			print '\tMATERIAL\t\t= (',
+			for item in x:
+				print '"' + item + '",',
+			print ')'
+			print '\t..'
+			print ''
 		else:
 			line = f.readline()
-
-	cLayers = cName + ' Layers'
-	print '"' + cName + '"' + " = CONSTRUCTION"
-	print '\tTYPE\t\t= LAYERS'
-	print '\tLAYERS\t\t= "' + cLayers + '"'
-	print '\t..'
-	print ''
-	print '"' + cLayers + '" = LAYERS'
-	print '\tMATERIAL\t\t= (',
-	for item in x:
-		print '"' + item + '",',
-	print ')'
-	print '\t..'
-	print ''
 
 main()
