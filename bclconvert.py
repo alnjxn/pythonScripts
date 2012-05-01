4556800c4d75464533d65c369ffffa5a84fc6682
Alan Jackson <a.jackson@case-inc.com>
2012-05-01 13:02:43 -0400

    added UI and AirGap function

--- bclconvert.py
+++ bclconvert.py
@@ -1,16 +1,51 @@
+from Tkinter import *
+from tkFileDialog import askopenfilename
+from tkFileDialog import asksaveasfilename
+import tkMessageBox
+import sys
 import re
 import StringIO
 
 def main():
-    f = open("/Users/ajackson/Desktop/epfile.idf")
-    o = open("/Users/ajackson/Desktop/out.txt", "w")
-    out = StringIO.StringIO()
-    out.write(trnCon(f, o))
-    out.write(trnMat(f, o))
-    #print >>o, out.getvalue()
-    print out.getvalue()
-    f.close()
+    try:
+        f = open(userDialog())
+        o = open(userSave(), 'w')
+        out = StringIO.StringIO()
+        out.write(trnMat(f, o))
+        out.write(trnAir(f, o))
+        out.write(trnCon(f, o))
+        print >>o, out.getvalue()
+        f.close()
+        o.close()
+    except (RuntimeError, TypeError, NameError):
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
 
+# Provide user with dialog box to select the IDF file to process
+def userDialog():
+    filename = askopenfilename(
+        filetypes=[('All Files','*'),('IDF Files','*.idf')], 
+        initialdir=['C:\\'], 
+        title='Select IDF File to Process ...')
+    if filename == "":
+        tkMessageBox.showinfo('Info', 'User Cancelled')
+    else:
+        return filename
+
+# Provide user with dialog box to select folder where processed
+# files should be placed
+def userSave():
+    savename = asksaveasfilename(
+        filetypes=[('All Files','*'),('INP Files','*.inp')],
+        initialdir = ['C:\\'],
+        title='Save Output INP As ...',
+        )
+    if savename == "":
+        tkMessageBox.showinfo("Info", "User Cancelled")
+    else:
+        return savename
+
+# Translate all Construction definitions
 def trnCon(f, o): 
     output = StringIO.StringIO()
     line = f.readline()
@@ -22,25 +57,21 @@ def trnCon(f, o):
         if re.search(str1, line):
             line = f.readline()
             cName = line.split(str3)[0].strip()
+            cLayers = cName + ' Layers'
             line = f.readline()
             
             while (not re.search(str2, line)):
                 x.append(line.split(str3)[0].strip())
                 line = f.readline()
             x.append(line.split(str2)[0].strip())
-
-            cLayers = cName + ' Layers'
-            output.write('"' + cName + '"' + " = CONSTRUCTION\n")
-            output.write('\tTYPE\t\t= LAYERS\n')
-            output.write('\tLAYERS\t\t= "' + cLayers + '"\n')
-            output.write('\t..\n\n')
+            
             output.write('"' + cLayers + '" = LAYERS\n')
             output.write('\tMATERIAL\t\t= ( ')
 
             cnt = len(x)
             for item in x:
                 if (cnt > 1):
-                    output.write('"' + item + '", ')
+                    output.write('"' + item + '", \n\t\t')
                 else:
                     output.write('"' + item + '" ')
                 cnt = cnt - 1
@@ -49,11 +80,17 @@ def trnCon(f, o):
             output.write(')\n')
             output.write('\t..\n\n')
 
+            output.write('"' + cName + '"' + " = CONSTRUCTION\n")
+            output.write('\tTYPE\t\t= LAYERS\n')
+            output.write('\tLAYERS\t\t= "' + cLayers + '"\n')
+            output.write('\t..\n\n')
+
         else:
             line = f.readline()
     f.seek(0)
     return output.getvalue()
 
+# Translate all Material definitions
 def trnMat(f, o):
     output = StringIO.StringIO()
     line = f.readline()
@@ -69,24 +106,52 @@ def trnMat(f, o):
             mRuff = line.split(str3)[0].strip()
             line = f.readline()
             mThck = line.split(str3)[0].strip()
-            mThck = float(mThck) * 3.28
+            mThck = float(mThck) * 3.280840
             line = f.readline()
             mCond = line.split(str3)[0].strip()
+            mCond = float(mCond) * 0.577789
             line = f.readline()
             mDens = line.split(str3)[0].strip()
+            mDens = float(mDens) * 0.062428
             line = f.readline()
             mSphe = line.split(str2)[0].strip()
+            mSphe = float(mSphe) * 0.000239
 
             output.write('"' + mName + '" = MATERIAL\n')
             output.write('\tTYPE\t\t\t= PROPERTIES\n')
             output.write('\tTHICKNESS\t\t= ' + str(mThck) + '\n')
-            output.write('\tCONDUCTIVITY\t= ' + mCond + '\n')
-            output.write('\tDENSITY\t\t\t= ' + mDens + '\n')
-            output.write('\tSPECIFIC-HEAT\t= ' + mSphe + '\n')
+            output.write('\tCONDUCTIVITY\t= ' + str(mCond) + '\n')
+            output.write('\tDENSITY\t\t\t= ' + str(mDens) + '\n')
+            output.write('\tSPECIFIC-HEAT\t= ' + str(mSphe) + '\n')
             output.write('\t..\n\n')
         else:
             line = f.readline()
-    
+    f.seek(0)
+    return output.getvalue()
+
+# Translate all AirGap definitions
+def trnAir(f, o):
+    output = StringIO.StringIO()
+    line = f.readline()
+    x = []
+    while line:
+        str1 = "Material:AirGap,"
+        str2 = ";"
+        str3 = ","
+        if re.search(str1, line):
+            line = f.readline()            
+            aName = line.split(str3)[0].strip()
+            line = f.readline() 
+            aRes = line.split(str2)[0].strip()
+            aRes = float(aRes) * 5.674467
+
+            output.write('"' + aName + '" = MATERIAL\n')
+            output.write('\tTYPE\t\t\t= RESISTANCE\n')
+            output.write('\tRESISTANCE\t\t= ' + str(aRes) + '\n')
+            output.write('\t..\n\n')
+        else:
+            line = f.readline()
+    f.seek(0)   
     return output.getvalue()
 
 if __name__ == '__main__':
